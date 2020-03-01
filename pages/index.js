38098a17254b02cc646c4b6ed8b390c07d60c2c3
arkinem <blazejgolinski@live.com>
2020-03-01 15:16:04 +0000

    intro card updates

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Layout from "../components/Layout";
 import styled from "styled-components";
 import IntroCard from "../components/cards/IntroCard";
 import ScheduleCard from "../components/cards/ScheduleCard";
+import AboutCard from "../components/cards/AboutCard";
 
 export default function Index() {
   return (
     <Layout>
       <IntroCard />
+      <AboutCard />
       <ScheduleCard />
     </Layout>
   );
