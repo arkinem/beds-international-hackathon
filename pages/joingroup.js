b8b34d59af94268a46f4327df611982a4b2db02a
arkinem <blazejgolinski@live.com>
2020-03-15 17:27:42 +0000

    initial commit

--- pages/joingroup.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import Layout from "../components/general/Layout";
-
-export default function JoinGroup() {
-  return <Layout>Join group</Layout>;
-}
---
+++ src/navigation/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import authentication from "../helpers/authentication";
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+export default ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        authentication.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
