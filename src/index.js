3af06065de40c3a2b4687b18d79a7b0e11babe90
arkinem <blazejgolinski@live.com>
2020-03-15 18:28:34 +0000

    adds helpers and css reset

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import AppRouter from "./navigation/AppRouter";
+import { GlobalStyle } from "./helpers/style";
+import Background from "./components/Background";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+const app = (
+  <>
+    <GlobalStyle />
+    <Background />
+    <AppRouter />
+  </>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
