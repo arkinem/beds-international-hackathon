e332739f651198388da2157a0833f1220446ded2
arkinem <blazejgolinski@live.com>
2020-05-13 03:19:34 +0100

    removes unused code

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -1,16 +1,9 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import LoginPage from "../pages/LoginPage";
 import LandingPage from "../pages/LandingPage";
 import DashboadPage from "../pages/DashboardPage";
-import authentication from "../helpers/authentication";
 import paths from "./paths";
 
 export default function AppRouter() {
