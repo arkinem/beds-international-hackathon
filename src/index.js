c74b2a9645369487dc3074f7894d7830e72098d7
arkinem <blazejgolinski@live.com>
2020-05-22 06:31:06 +0100

    removes service worker comments

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import * as serviceWorker from "./serviceWorker";
 import AppRouter from "./navigation/AppRouter";
 import { GlobalStyle } from "./constants/layout";
 
-serviceWorker.unregister();
-
 const app = (
   <>
     <GlobalStyle />
@@ -14,7 +12,4 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
