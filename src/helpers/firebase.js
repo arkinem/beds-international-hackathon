77ede8f00ea7a023b59fa1a4b47d5abcbce9c137
arkinem <blazejgolinski@live.com>
2020-05-31 03:17:32 +0100

    adds firebase auth

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,10 +1,11 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 import firebaseConfig from "../constants/firebaseConfig";
 
 firebase.initializeApp(firebaseConfig);
 
-// const auth = firebase.auth();
+export const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 const universitiesCollection = firestore.collection(`universities`);
