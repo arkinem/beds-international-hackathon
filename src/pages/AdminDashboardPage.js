e84baa8cdace7af648799030c8747ffbba904396
arkinem <blazejgolinski@live.com>
2020-05-31 04:37:36 +0100

    adds temp logout

--- src/pages/AdminDashboardPage.js
+++ src/pages/AdminDashboardPage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PageContainer from "../components/PageContainer";
+import { auth } from "../helpers/firebase";
+import { Button } from "antd";
 
 class AdminDashboardPage extends React.Component {
   render() {
-    return <PageContainer title={"Admin"}>dashboard</PageContainer>;
+    return (
+      <PageContainer title={"Admin"} hideLeftSection>
+        <Button onClick={() => auth.signOut()}>logout</Button>
+      </PageContainer>
+    );
   }
 }
 
